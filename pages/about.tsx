0c534e38fc8bd7c1d2764b2d4a7d7d1147ea5346
Anubhav Dhuliya <dhuliya.a@gmail.com>
2023-03-13 21:42:53 +0530

    Projects and about sections

--- pages/about.tsx
+++ pages/about.tsx
@@ -13,9 +13,16 @@ export default function About() {
       <div className={styles.about}>
         <div className={styles.col1}></div>
         <div className={styles.col2}>
-          <div className={styles.row1}></div>
-          <div className={styles.row2}></div>
-          <div className={styles.row3}></div>
+          <div className={styles.row1}>
+            <div className={styles.line}><hr className={styles.line}/></div>
+            <div className={styles.title}>ABOUT</div>
+          </div>
+          <div className={styles.row2}>
+            <p>Hi there! My name is Anubhav Dhuliya and I'm a software engineer and creator based in New Delhi, India. My passion lies in using technology to tackle societal issues and crafting innovative solutions for the web.</p>
+            <p>Currently working as a Senior Software Engineer at Optum, I did my Bachelor’s in Computer Science & Engineering from Shiv Nadar University, Delhi-NCR. In the past, I have gained valuable work experience in various positions such as a Research Trainee at Defence R&D Organization of India where I utilized a Super-Resolution Generative Adversarial Network (SRGAN) to address the challenge of super-resolution and refining jointly. My product-based experiences include my work as a Full-Stack Software Engineer at FloBiz where I developed tools for Indian businesses and as a front-end engineer at Troopr.ai, where I created custom UI components and features.</p>
+            <p> During my free time, I enjoy playing football, pursuing personal projects such as developing applications, creating films, and writing poems, and going for a run.</p>
+          </div>
+          <div className={styles.row3}>RESUME</div>
         </div>
       </div>
     </div>
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -11,10 +11,42 @@ export default function Projects() {
     <>
     <div className={styles.container}>
       <div className={styles.projects}>
-        <div className={styles.row1}></div>
-        <div className={styles.row2}></div>
+        <div className={styles.row1}>
+        <div className={styles.title}>SELECTED PROJECTS</div>
+          <div className={styles.line}><hr className={styles.line2}/></div>
+        </div>
+        <div className={styles.row2}>Take a look at some of the projects I've built.</div>
         <div className={styles.row3}>
-          <div className={styles.col1}></div>
+          <div className={styles.col1}>
+            <div className={styles.project}>
+              <div className={styles.projectTitle}>Layermint</div>
+              <div className={styles.projectBody}>Project @ The Untitled Collective<br/>
+                                                  Full-Stack Developer<br/>
+                                                  An image generation & NFT-minting tool using layer assets to create your own collection.
+              </div>
+            </div>
+            <div className={styles.project}>
+              <div className={styles.projectTitle}>Layermint</div>
+              <div className={styles.projectBody}>Project @ The Untitled Collective<br/>
+                                                  Full-Stack Developer<br/>
+                                                  An image generation & NFT-minting tool using layer assets to create your own collection.
+              </div>
+            </div>
+            <div className={styles.project}>
+              <div className={styles.projectTitle}>Layermint</div>
+              <div className={styles.projectBody}>Project @ The Untitled Collective<br/>
+                                                  Full-Stack Developer<br/>
+                                                  An image generation & NFT-minting tool using layer assets to create your own collection.
+              </div>
+            </div>
+            <div className={styles.project}>
+              <div className={styles.projectTitle}>Layermint</div>
+              <div className={styles.projectBody}>Project @ The Untitled Collective<br/>
+                                                  Full-Stack Developer<br/>
+                                                  An image generation & NFT-minting tool using layer assets to create your own collection.
+              </div>
+            </div>
+          </div>
           <div className={styles.col2}></div>
         </div>
       </div>
