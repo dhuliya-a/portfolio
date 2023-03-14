07a4c672097ba6c82b23a7d95ce6587d3fa8034d
Anubhav Dhuliya <dhuliya.a@gmail.com>
2023-03-15 00:51:38 +0530

    CTA section

--- pages/cta.tsx
+++ pages/cta.tsx
@@ -12,15 +12,26 @@ export default function Cta() {
     <div className={styles.container}>
       <div className={styles.cta}>
           <div className={styles.row1}>
-            <div className={styles.header}></div>
-            <div className={styles.line}></div>
+            <div className={styles.title}>LET'S CONNECT</div>
+            <div className={styles.line}><hr className={styles.line2}/></div>
           </div>
-          <div className={styles.row2}></div>
+          <div className={styles.row2}>Feel free to drop a message if you have any questions or would like to collaborate on a project!</div>
           <div className={styles.row3}>
-            <div className={styles.socials}></div>
-            <div className={styles.form}></div>
+            <div className={styles.socials}>
+              <p className={styles.socialsTitle}>SOCIALS</p>
+              <p className={styles.socialsLink}>GitHub</p>
+              <p className={styles.socialsLink}>LinedIn</p>
+              <p className={styles.socialsLink}>Gmail</p>
+              <p className={styles.socialsLink}>Instagram</p>
+            </div>
+            <form className={styles.form}>
+              <input placeholder='Enter email'></input>
+              <input placeholder='Enter subject'></input>
+              <textarea placeholder='Your message'></textarea>
+              <button>Submit</button>
+            </form>
           </div>
-          <div className={styles.submit}></div>
+          {/* <div className={styles.submit}></div> */}
       </div>
     </div>
   </>
