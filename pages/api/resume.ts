2d3f284f9d7d7fea48f09b3082ffac0f06d34539
Anubhav Dhuliya <dhuliya.a@gmail.com>
2023-04-10 23:30:47 +0530

    Responsive updates

--- pages/api/resume.ts
+++ pages/api/resume.ts
@@ -1,12 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import type { NextApiRequest, NextApiResponse } from 'next'
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  // const file = require("../../public/resume.pdf");
-  res.setHeader('Content-Type', 'application/pdf');
-  res.send(file);
-  // res.status(200).json({ name: 'John Doe' })
-}
+// import type { NextApiRequest, NextApiResponse } from 'next'
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   // const file = require("../../public/resume.pdf");
+//   res.setHeader('Content-Type', 'application/pdf');
+//   res.send(file);
+//   // res.status(200).json({ name: 'John Doe' })
+// }
